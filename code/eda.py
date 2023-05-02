9b840f7d3d3d2b2f1c4590b3c8e6e6c22187e61f
Herman F. Autore <herman@ufl.edu>
2023-05-02 16:19:33 -0400

    Update

--- code/eda.py
+++ code/eda.py
@@ -17,7 +17,7 @@ LOG_LEVEL = "INFO"
 DATA_PATH = Path("data/input/vehicles.csv")
 
 # Settings: Interactive Pyplot
-# get_ipython().run_line_magic('matplotlib', "")
+get_ipython().run_line_magic('matplotlib', "")
 
 # Variables: Path construction: General
 runTimestamp = getTimestamp()
@@ -68,12 +68,11 @@ if __name__ == "__main__":
     data = pd.read_csv(DATA_PATH)
     logging.info("Finished reading data")
 
-    # Analysis for `price`
-    VARIABLE_NAME = 'price'
-    x = data[VARIABLE_NAME]
-    logging.info(f"Analyzing {VARIABLE_NAME}")
+    # Analysis for `price`: Round 1
+    x = data['price']
+    logging.info(f"Analyzing {'price'}")
 
-    # Manually determine bin widths for `VARIABLE_NAME`
+    # Manually determine bin widths for `'price'`
     logging.info("  Getting five-number summary.")
     x1 = x.min()
     x2 = x.quantile(.25)
@@ -95,35 +94,71 @@ if __name__ == "__main__":
                      '99th ptile',
                      'max']
     summaryStats = pd.DataFrame([sumStatsValues], columns=sumStatsNames).T
-    logging.info(f"Summary statistics for {VARIABLE_NAME}:\n{summaryStats.__str__()}")
+    logging.info(f"Summary statistics for {'price'}:\n{summaryStats.__str__()}")
 
+    # Boxplot
     fig1 = plt.figure()
     fignum = fig1.number
-    fig = plt.boxplot(data[VARIABLE_NAME])
+    fig = plt.boxplot(data['price'])
 
-    # Scatterplots
-    logging.info("Plotting scatterplots")
+    # Histogram
+    logging.info("Plotting histograms")
+    fig2 = plt.figure()
+    fignum = fig2.number
+    n, bins, array = plt.hist(data['price'])
+    # NOTE `n` reveals that there are 4 + 2 + 3 + 2 = 11 values that are outside the first bin. We can create one bin from the nine right-most bins, and then split up the first bin. We are justified in removing these outliers because we are confident they do not represent actual variable values, but erroneous data inputs. The values are most likely phone numbers.
+    # array([4.26869e+05, 0.00000e+00, 4.00000e+00, 2.00000e+00, 0.00000e+00,
+#    0.00000e+00, 0.00000e+00, 0.00000e+00, 3.00000e+00, 2.00000e+00])
+    plt.show()
+
+    # Remove outliers (first try)
+    NUM_OUTLIERS = 11
+    x_new = x.sort_values()[:len(x) - 1 - NUM_OUTLIERS]
+    
+    # NOTE When we remove the top 11 values we still notice that there are some unusual variable values, namely sequential or repeating values. The values are more unusual as we go from the top 100 to the top 20 values. We can make notes of these cases and revisit them in the future. Perhaps there is a reason why the input price value is as exceptional as it is. In the meantime we will only drop the values which made the visualization unsuable, i.e., the top 11 values.
+
+    priceOutliers = x.sort_values()[-NUM_OUTLIERS:]
+
+    # Analysis for `price`: Round 2
+    logging.info(f"Analyzing {'price'} without outliers")
+
+    # Manually determine bin widths for `'price'`
+    logging.info("  Getting five-number summary.")
+    x1 = x_new.min()
+    x2 = x_new.quantile(.25)
+    x3 = x_new.quantile(.50)
+    x4 = x_new.mean()
+    x5 = x_new.quantile(.75)
+    x6 = x_new.quantile(.90)
+    x7 = x_new.quantile(.95)
+    x8 = x_new.quantile(.99)
+    x9 = x_new.max()
+    sumStatsValues = [x1, x2, x3, x4, x5, x6, x7, x8, x9]
+    sumStatsNames = ['min',
+                     '25th ptile',
+                     'median',
+                     'mean',
+                     '75th ptile',
+                     '90th ptile',
+                     '95th ptile',
+                     '99th ptile',
+                     'max']
+    summaryStats = pd.DataFrame([sumStatsValues], columns=sumStatsNames).T
+    logging.info(f"Summary statistics for {'price'}:\n{summaryStats.__str__()}")
+
+    # Boxplot
     fig1 = plt.figure()
     fignum = fig1.number
-    fig = plt.scatter(data[VARIABLE_NAME], [1] * len(data))
-    plt.xlabel(VARIABLE_NAME)
-    plt.title('One-Dimensional Scatter Plot')  # (boxplot without box, haha)
-    plt.legend(loc="lower right")
+    fig = plt.boxplot(x_new)
 
-    if True:
-        # Histogram
-        logging.info("Plotting histograms")
-        # bins = None  # TODO Can get bin widths using `summaryStats` as starting point
-        fig2 = plt.figure()
-        fignum = fig2.number
-        # NOTE for `bins` I already tried `10`, `1000`, and `20000`. The lines are so thin I can't see them.
-        # bins = [x1, x2, x3, x5, x6, x7, x8, x9]  # All same height
-        # n, bins, array = plt.hist(data[VARIABLE_NAME], bins='fd')  # NOTE WARNING Freezes computer
-        # n, bins, array = plt.hist(data[VARIABLE_NAME], log=True)  # Best result so far
-        n, bins, array = plt.hist(data[VARIABLE_NAME])
-        # array([4.26869e+05, 0.00000e+00, 4.00000e+00, 2.00000e+00, 0.00000e+00,
-    #    0.00000e+00, 0.00000e+00, 0.00000e+00, 3.00000e+00, 2.00000e+00])  # NOTE That there are 4+2+3+2=11 values that are outside the first bin. I sohuld create one bin from the nine right-most bins, and then split up the first bin. The first bin has the following edges: array([0.00000000e+00, 3.73692871e+08]) TODO
-        plt.show()
+    # Histogram
+    logging.info("Plotting histograms")
+    fig2 = plt.figure()
+    fignum = fig2.number
+    n, bins, array = plt.hist(x_new)
+    plt.show()
+
+    # NOTE: After the second round of visualization, we see that the data is still very skewed to the right. This time we have 10 values which are outside the first bin. I should create a function for this and repeat until I get a usuable visualization.
 
     # End script
     logging.info(f"""Finished running "{thisFilePath.relative_to(projectDir)}".""")
